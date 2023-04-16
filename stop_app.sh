8bfdd3b2477400d77b1fcc3f7ec3382206b9c858
Heavybullets8 <heavybullets8@gmail.com>
2023-04-15 18:26:20 -0600

    new allowed test

--- stop_app.sh
+++ stop_app.sh
@@ -28,7 +28,7 @@ handle_stop_code() {
             return 1
             ;;
         3)
-            echo "HeavyScript doesn't have the ability to stop Prometheus"
+            echo "HeavyScript doesn't have the ability to stop Operators"
             return 1
             ;;
         4)
@@ -57,16 +57,14 @@ stop_app() {
     app_name="$1"
     timeout="150"
 
-    # Grab chart info
-    chart_info=$(midclt call chart.release.get_instance "$app_name")
-
-    output=$(check_filterd_apps)
+    # Check if app is a cnpg instance, or an operator instance
+    output=$(check_filtered_apps "$app_name")
 
     # Check if output contains the desired namespace and "cnpg"
     if echo "$output" | grep -q "ix-${app_name},cnpg"; then
         return 4
-    # Check if app is a prometheus instance
-    elif printf "%s" "$chart_info" | grep -sq -- \"prometheus\":;then
+    # Check if app is an operator instance
+    elif echo "$output" | grep -q "ix-${app_name},operator"; then
         return 3
     fi
 
