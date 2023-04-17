408f0c35220003845cb67e0e49e358ad84233458
heavybullets8 <heavybullets8@gmail.com>
2023-04-17 16:44:05 -0600

    grab volume name

--- migrate.sh
+++ migrate.sh
@@ -142,6 +142,9 @@ get_pvc_info() {
 }
 
 get_pvc_parent_path() {
+    local volume_name
+    volume_name=$(echo "$pvc_info" | awk '{print $2}' | head -n 1)
+
     pvc_path=$(zfs list -r "${ix_apps_pool}" -o name -H | grep "${volume_name}")
 
     if [ -z "${pvc_path}" ]; then
