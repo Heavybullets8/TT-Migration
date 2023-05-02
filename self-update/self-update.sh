f01750063a83a4f20e8a109e0c27d0a4058bf5ee
Heavybullets8 <heavybullets8@gmail.com>
2023-05-02 01:44:42 -0600

    silence output

--- self-update/self-update.sh
+++ self-update/self-update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 auto_update_script() {
-    git -C "$script_path" fetch
+    git -C "$script_path" fetch > /dev/null 2>&1
 
     if ! git -C "$script_path" diff --quiet origin/"$current_version"; then
         echo -e "${yellow}Updating the script to the latest version...${reset}"
         
-        if git -C "$script_path" pull; then
+        if git -C "$script_path" pull > /dev/null 2>&1; then
             echo -e "${green}Script updated successfully! Rerunning the script with the update.${reset}"
             exec bash "$script_path/$script_name" "${args[@]}"
         else
