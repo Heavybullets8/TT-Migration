590133dbcfba4cfc84454e9643641617ada92225
Heavybullets8 <heavybullets8@gmail.com>
2023-05-02 01:53:12 -0600

    newline character after update message

--- self-update/self-update.sh
+++ self-update/self-update.sh
@@ -8,11 +8,10 @@ auto_update_script() {
         echo -e "${yellow}Updating the script to the latest version...${reset}"
         
         if git -C "$script_path" pull > /dev/null 2>&1; then
-            echo -e "${green}Script updated successfully! Rerunning the script with the update.${reset}"
+            echo -e "${green}Script updated successfully! Rerunning the script with the update.${reset}\n"
             exec bash "$script_path/$script_name" "${args[@]}"
         else
-            echo -e "${red}Error: Failed to update the script. Please check your repository or try again later.${reset}"
+            echo -e "${red}Error: Failed to update the script. Please check your repository or try again later.${reset}\n"
         fi
-        echo
     fi
 }
