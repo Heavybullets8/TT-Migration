4d07f944ea5ed12ce0ffa218283f4b89e333dd82
Heavybullets8 <heavybullets8@gmail.com>
2023-05-02 02:07:31 -0600

    tack on no update

--- self-update/self-update.sh
+++ self-update/self-update.sh
@@ -9,7 +9,7 @@ auto_update_script() {
         
         if git -C "$script_path" pull > /dev/null 2>&1; then
             echo -e "${green}Script updated successfully! Rerunning the script with the update.${reset}\n"
-            exec bash "$script_path/$script_name" "${args[@]}"
+            exec bash "$script_path/$script_name" "${args[@]}" --no-update
         else
             echo -e "${red}Error: Failed to update the script. Please check your repository or try again later.${reset}\n"
         fi
