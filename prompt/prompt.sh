98de1c3a022fcb779368a77621740dc5662a6c72
heavybullets8 <heavybullets8@gmail.com>
2023-05-02 14:19:27 -0600

    initial

--- prompt/prompt.sh
+++ prompt/prompt.sh
@@ -5,15 +5,24 @@ prompt_app_name() {
     do
         # Prompt the user for the app name
         read -r -p "Enter the application name: " appname
-        namespace="ix-${appname}"
 
-        # Check if the app exists
-        if check_if_app_exists "${appname}"; then
-            echo -e "${green}Found: ${blue}${appname}${reset}"
-            echo
-            break
+        # Check if the appname fits within the regex
+        if [[ "${appname}" =~ ^[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9])?$ ]]; then
+            # Convert the appname to lowercase
+            appname_lowercase=$(echo "${appname}" | tr '[:upper:]' '[:lower:]')
+            appname="${appname_lowercase}"
+            namespace="ix-${appname}"
+
+            # Check if the app exists
+            if check_if_app_exists "${appname}"; then
+                echo -e "${green}Found: ${blue}${appname}${reset}"
+                echo
+                break
+            else
+                echo -e "${yellow}Error: App not found.${reset}"
+            fi
         else
-            echo -e "${yellow}Error: App not found.${reset}"
+            echo -e "${yellow}\"${new_appname}\" is not valid. Please enter a valid name.${reset}"
         fi
     done
 }
@@ -54,11 +63,22 @@ prompt_rename() {
 }
 
 rename_app() {
-    # Prompt the user for the new app name
-    read -r -p "Enter the new application name: " new_appname
+    while true; do
+        # Prompt the user for the new app name
+        read -r -p "Enter the new application name: " new_appname
 
-    appname="${new_appname}"
-    namespace="ix-${appname}"
+        # Check if the new_appname fits within the regex
+        if [[ "${new_appname}" =~ ^[a-zA-Z]([-a-zA-Z0-9]*[a-zA-Z0-9])?$ ]]; then
+            # Convert the new_appname to lowercase
+            new_appname_lowercase=$(echo "${new_appname}" | tr '[:upper:]' '[:lower:]')
+
+            appname="${new_appname_lowercase}"
+            namespace="ix-${appname}"
+            break
+        else
+            echo -e "${yellow}\"${new_appname}\" is not valid. Please enter a valid name.${reset}"
+        fi
+    done
 }
 
 prompt_migration_path() {
