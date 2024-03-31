0259e74d026eb41b19985a0fc04a023f3aa31c7d
Heavy <heavybullets8@gmail.com>
2024-03-31 16:40:13 -0600

    Update stop_app function to include a direct flag

--- lifecycle/lifecycle.sh
+++ lifecycle/lifecycle.sh
@@ -8,7 +8,7 @@ stop_app_if_needed() {
                 awk -F ',' '{print $2}'| 
                 sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
     if [[ "$status" != "STOPPED" ]]; then
-        stop_app "$appname"
+        stop_app "direct" "$appname"
         result=$(handle_stop_code "$?")
         if [[ $? -eq 1 ]]; then
             echo -e "${red}${result}${reset}"
