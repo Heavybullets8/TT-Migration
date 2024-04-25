c59a2f09c41a55ee402c9a1723b3e02a930d279d
Heavy <heavybullets8@gmail.com>
2024-04-25 13:17:12 -0600

    Refactor variable names and update JSON file paths in migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -103,10 +103,10 @@ main() {
         # import_variables
         source "/mnt/$migration_path/variables.txt" 
 
-        if [[ -f "${backup_path}/pvcs_old.json" ]]; then
-            jq -c '.[] | select(((.pvc_name | test("-cnpg-"; "i")) or (.pvc_name | endswith("-redis-0"))) and .ignored == false)' "${backup_path}/pvcs_old.json" | while read -r pvc; do
+        if [[ -f "${backup_path}/pvcs_original.json" ]]; then
+            jq -c '.[] | select(((.pvc_name | test("-cnpg-"; "i")) or (.pvc_name | endswith("-redis-0"))) and .ignored == false)' "${backup_path}/pvcs_original.json" | while read -r pvc; do
                 pvc_name=$(echo "$pvc" | jq -r '.pvc_name')
-                update_json_file "${backup_path}/pvcs_old.json" \
+                update_json_file "${backup_path}/pvcs_original.json" \
                                 ".pvc_name == \"$pvc_name\"" \
                                 ".ignored = true"
             done
