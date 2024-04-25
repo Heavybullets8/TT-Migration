4667b14ba0a1d4d3e91e75cedc5ed92926a1843a
Heavy <heavybullets8@gmail.com>
2024-04-25 13:43:54 -0600

    Update delete_original_app function in lifecycle.sh

--- lifecycle/lifecycle.sh
+++ lifecycle/lifecycle.sh
@@ -67,8 +67,15 @@ delete_original_app() {
             echo -e "\nPlease ensure all pods are terminated and no resources are in use before trying again."
             echo -e "Rerun the script with ${blue}--skip${reset} after you are certain everything is deleted."
         fi
-        echo -e "As a last resort you can attempt to unset your apps pool, or restart your server and then attempt the delete command again"
-        echo -e "Prior to doing either of those, please check with a support member to see if it is necessary."
+        echo -e "\n${bold}What to do if your dataset is reporting busy:${reset}"
+        echo -e "Try the following in these EXACT steps, deviation of any kind can result in data loss:"
+        echo -e "1. Try to delete the dataset using the command ${blue}zfs destroy -r \"${dataset}\"${reset}"
+        echo -e "2. If Step 1 fails, note the name of the failed app(s) and continue migrating ALL remaining apps."
+        echo -e "3. After all apps are migrated, unset the apps pool: TrueNAS SCALE GUI -> Applications -> Settings -> Unset Pool"
+        echo -e "4. Go through your list of failed apps and delete them one by one example: ${blue}zfs destroy -r \"${dataset}\"${reset}"
+        echo -e "5. After all failed apps datasets are deleted, set the apps pool: TrueNAS SCALE GUI -> Applications -> Settings -> Set Pool"
+        echo -e "6. Re-run the script with ${blue}--skip${reset} flag for all failed apps."
+        echo -e "\nIt is very important to attempt all migrations until unsetting the applications pool, unsetting the applications pool, or restarting with applicaitons mid-migration can result in data loss."
         return 1
     else
         echo -e "${green}Success${reset}\n"
