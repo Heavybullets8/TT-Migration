537d871a6e3fcab89798e9fe92025b104a09772e
Heavy <heavybullets8@gmail.com>
2024-04-25 12:56:27 -0600

    ignore cnpg and redis retroactively

--- migrate.sh
+++ migrate.sh
@@ -102,6 +102,29 @@ main() {
         prompt_migration_path || exit 1
         # import_variables
         source "/mnt/$migration_path/variables.txt" 
+
+        if [[ -f "${backup_path}/pvcs_old.json" ]]; then
+            jq -c '.[] | select(((.pvc_name | test("-cnpg-"; "i")) or (.pvc_name | endswith("-redis-0"))) and .ignored == false)' "${backup_path}/pvcs_old.json" | while read -r pvc; do
+                pvc_name=$(echo "$pvc" | jq -r '.pvc_name')
+                update_json_file "${backup_path}/pvcs_old.json" \
+                                ".pvc_name == \"$pvc_name\"" \
+                                ".ignored = true"
+            done
+        fi
+
+        if [[ -f "${backup_path}/pvcs_new.json" ]]; then
+            jq -c '.[] | select(((.pvc_name | test("-cnpg-"; "i")) or (.pvc_name | endswith("-redis-0"))) and .ignored == false)' "${backup_path}/pvcs_new.json" | while read -r pvc; do
+                pvc_name=$(echo "$pvc" | jq -r '.pvc_name')
+                update_json_file "${backup_path}/pvcs_new.json" \
+                                ".pvc_name == \"$pvc_name\"" \
+                                ".ignored = true"
+            done
+        fi
+
+        update_json_file "$new_app_pvc_info" \
+                         ".pvc_name == \"$new_pvc_name\"" \
+                         ".matched = true"
+
     fi
 
     case $script_progress in
