ddd36adc81ed0ecb687301e996853f9f2ef467b0
Heavy <heavybullets8@gmail.com>
2024-05-01 01:34:42 -0600

    update marker

--- create/create_marker.py
+++ create/create_marker.py
@@ -4,18 +4,21 @@ import base64
 import hashlib
 import time
 import json
+from datetime import datetime
 
-def create_marker(username, backup_path, **kwargs):
-    timestamp = time.time()
-    states = json.dumps(kwargs)
-    encoded_states = base64.b64encode(states.encode()).decode()
-
-    secret_string = secrets.token_urlsafe(16)
-    encoded_string = base64.b64encode(secret_string.encode())[::-1]
-    combined_content = f"{timestamp}:{username}:{encoded_string}:{encoded_states}"
-    hash_of_content = hashlib.sha256(combined_content.encode()).hexdigest() 
-    marker_content = f"{hash_of_content}:{combined_content}"
-    marker_filename = f".marker_{hash_of_content[:8]}"
+def create_marker(username, app_name, namespace, backup_path, chart_name, **kwargs):
+    timestamp = datetime.utcnow().isoformat() 
+    states = json.dumps(kwargs)  
+    
+    encoded_backup_path = base64.urlsafe_b64encode(backup_path.encode()).decode()
+    secret_component = secrets.token_urlsafe(16)  # A secret component to add complexity
+    components = f"{timestamp}:{username}:{app_name}:{namespace}:{encoded_backup_path}:{chart_name}:{secret_component}"
+    
+    combined_hash = hashlib.sha256(components.encode()).hexdigest()
+    
+    marker_content = f"{combined_hash}:{timestamp}:{username}:{app_name}:{namespace}:{encoded_backup_path}:{chart_name}:{states}"
+    
+    marker_filename = f".marker_{hashlib.sha256(marker_content.encode()).hexdigest()[:8]}"
     marker_path = os.path.join(backup_path, marker_filename)
 
     with open(marker_path, "w") as f:
@@ -23,14 +26,19 @@ def create_marker(username, backup_path, **kwargs):
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) < 6:
-        print("Usage: python create_marker.py <username> <backup_path> <force> <outdated> <deploying>")
+    if len(sys.argv) < 10:
+        print("Usage: python create_marker.py <username> <app_name> <namespace> <backup_path> <chart_name> <force> <outdated> <deploying> <migrate_db> <migrate_pvs>")
         sys.exit(1)
 
     username = sys.argv[1]
-    backup_path = sys.argv[2]
-    force = sys.argv[3]
-    outdated = sys.argv[4]
-    deploying = sys.argv[5]
+    app_name = sys.argv[2]
+    namespace = sys.argv[3]
+    backup_path = sys.argv[4]
+    chart_name = sys.argv[5]
+    force = sys.argv[6]
+    outdated = sys.argv[7]
+    deploying = sys.argv[8]
+    migrate_db = sys.argv[9]
+    migrate_pvs = sys.argv[10]
     
-    create_marker(username, backup_path, force=force, outdated=outdated, deploying=deploying)
+    create_marker(username, app_name, namespace, backup_path, chart_name, force=force, outdated=outdated, deploying=deploying, migrate_db=migrate_db, migrate_pvs=migrate_pvs)
